bea53a26cb29ae4ccc8b6644fac2d81c5568402f
7RonMor7 <ronald7moreno7@gmail.com>
2026-09-18 13:00:54 -0500

    Add: Repositorio 'Usuario'

---
+++ backend/src/main/java/com/petcare/usuarios/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.petcare.usuarios.repository;
+
+import com.petcare.usuarios.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByCorreoIgnoreCase(String correo);
+
+    Optional<Usuario> findByCorreoIgnoreCase(String correo);
+}
