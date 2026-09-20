3e7f47680481444c57d24e1509ee094f0b510b1c
7RonMor7 <ronald7moreno7@gmail.com>
2026-09-20 17:02:55 -0500

    Add: Entidad 'Permiso'

---
+++ backend/src/main/java/com/petcare/usuarios/domain/Permiso.java
@@ -0,0 +1,22 @@
+package com.petcare.usuarios.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "permiso")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Permiso {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, unique = true, length = 60)
+    private String nombre;
+
+    @Column(length = 200)
+    private String descripcion;
+}
