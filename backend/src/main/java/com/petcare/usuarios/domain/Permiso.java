5de7dd2c93967fc066720fa66d01815b5a7ed81d
7RonMor7 <ronald7moreno7@gmail.com>
2026-09-20 19:28:01 -0500

    Update: En la entidad permiso

--- backend/src/main/java/com/petcare/usuarios/domain/Permiso.java
+++ backend/src/main/java/com/petcare/usuarios/domain/Permiso.java
@@ -13,7 +13,7 @@ public class Permiso {
 
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(nullable = false, unique = true, length = 60)
     private String nombre;
 
