883bd267848bc192f187c63f1fe15f27a3b911f1
7RonMor7 <ronald7moreno7@gmail.com>
2026-09-22 15:42:28 -0500

    Add: Componente que decide si la persona puede pasar

---
+++ frontend/src/auth/RutaProtegida.jsx
@@ -0,0 +1,22 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import SinPermisosPage from "../pages/SinPermisosPage";
+
+export default function RutaProtegida({ permiso }) {
+    const { estaAutenticado, cargando, tienePermiso } = useAuth();
+    const location = useLocation();
+
+    if (cargando){
+        return <p className="p-6 text-neutral-600">Cargando...</p>;
+    }
+
+    if (!estaAutenticado) {
+        return <Navigate to="/login" replace state={{ desde: location.pathname }}  />;
+    }
+
+    if (permiso && !tienePermiso(permiso)) {
+        return <SinPermisosPage />;
+    }
+
+    return <Outlet />;
+}
