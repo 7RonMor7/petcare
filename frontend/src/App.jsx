43a0aab96f8159c26dd36c64c00dc1d528fdf7d7
7RonMor7 <ronald7moreno7@gmail.com>
2026-09-22 15:46:18 -0500

    Update: Agregación de rutas anidadas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import RutaProtegida from "./components/RutaProtegida";
+import DisenoApp from "./components/DisenoApp";
 import LoginPage from "./pages/LoginPage";
 import RegistroPage from "./pages/RegistroPage";
 import InicioPage from "./pages/InicioPage";
@@ -6,9 +8,17 @@ import InicioPage from "./pages/InicioPage";
 export default function App() {
   return (
     <Routes>
+      {/* Públicas  */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/registro" element={<RegistroPage />} />
-      <Route path="/" element={<InicioPage />} />
+
+      {/* Privadas: primero el guardián, después el marco con la barra */}
+      <Route element={<RutaProtegida />}>
+        <Route element={<DisenoApp />}>
+          <Route path="/" element={<InicioPage />} />
+        </Route>
+      </Route>
+
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
