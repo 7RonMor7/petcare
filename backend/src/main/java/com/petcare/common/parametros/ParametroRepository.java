85c64f61250d4bbf1d4c9487728fdc11dbe0b4ed
7RonMor7 <ronald7moreno7@gmail.com>
2026-09-25 14:24:33 -0500

    Add: Repositorio para los Parametros

---
+++ backend/src/main/java/com/petcare/common/parametros/ParametroRepository.java
@@ -0,0 +1,6 @@
+package com.petcare.common.parametros;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParametroRepository extends JpaRepository<ParametroSistema, String> {
+}
